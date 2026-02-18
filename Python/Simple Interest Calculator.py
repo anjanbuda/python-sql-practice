e4e93950d8af2b921d3027f5658befe3035fcc64
Anjan Buda <anjan.buda@gmail.com>
2026-02-18 23:41:19 +0530

    Simple Interest Calculatot

--- Python/Simple Interest Calculator.py
+++ Python/Simple Interest Calculator.py
@@ -4,8 +4,10 @@ Write a program that calculates the simple interest for a given principal amount
 The formula for simple interest is: Simple Interest = (principal * rate * time) / 100.
 Take principal ,rate , time from user.
 '''
-principal=float(input("Enter the principal amount: "))
-rate=float(input("Enter the rate of interest: "))
+principal=float(input("Enter the principal amount:₹"))
+rate=float(input("Enter the rate of interest:₹"))
 time=int(input("Enter the time in year : "))
 simple_int=(principal*rate*time)/100
-print("The simple interest is: ",simple_int)
+maturity=principal+simple_int
+print(f"The intreast amount is ₹ {simple_int:0.2f}")
+print(f"The maturity is ₹ {maturity:0.2f}")
